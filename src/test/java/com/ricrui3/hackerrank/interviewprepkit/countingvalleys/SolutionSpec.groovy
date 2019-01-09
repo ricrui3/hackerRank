5861071bbec6d672b87a375ffd1a4524d06d8b6c
Ricardo Ruiz <ricrui3@gmail.com>
2019-01-09 10:58:55 -0600

    Spock Spec of the "Counting Valleys" challenge

---
+++ src/test/java/com/ricrui3/hackerrank/interviewprepkit/countingvalleys/SolutionSpec.groovy
@@ -0,0 +1,20 @@
+package com.ricrui3.hackerrank.interviewprepkit.countingvalleys
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SolutionSpec extends Specification {
+
+    @Unroll
+    def "Counting valley challenge, trip: #trip"() {
+        when:
+        def valleyCount = Solution.countingValleys(size, trip)
+        then:
+        valleyCountExpected == valleyCount
+        where:
+        size | trip                               | valleyCountExpected
+        5    | "DDUUD"                            | 1
+        32   | "DUUUUDUDUDUDUDDUDDDDDUUUUUUUUUUU" | 2
+        8    | "UUUUDDDD"                         | 0
+    }
+}
