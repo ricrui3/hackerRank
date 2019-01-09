17be65192659f90d0530eec627e501faec9e304c
Ricardo Ruiz <ricrui3@gmail.com>
2019-01-09 09:00:33 -0600

    Initial commit and Solution to the counting valleys challenge

---
+++ src/main/java/com/ricrui3/hackerrank/interviewprepkit/countingvalleys/README.md
@@ -0,0 +1,2 @@
+Solution to the 'Counting Valleys' challenge
+Link: https://www.hackerrank.com/challenges/counting-valleys/problem
---
+++ src/main/java/com/ricrui3/hackerrank/interviewprepkit/countingvalleys/Solution.java
@@ -0,0 +1,54 @@
+package com.ricrui3.hackerrank.interviewprepkit.countingvalleys;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        char[] trip = s.toCharArray();
+        boolean isValley = false;
+        int altitude = 0;
+        int valleyCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (trip[i] == 'D') {
+                altitude--;
+                if (altitude == -1) {
+                    isValley = true;
+                }
+            } else {
+                altitude++;
+                if (altitude == 0 && isValley) {
+                    valleyCount++;
+                    isValley = false;
+                }
+            }
+        }
+        return valleyCount;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter("./src/main/java/com/ricrui3/hackerrank/interviewprepkit/countingvalleys/solution.txt"));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
