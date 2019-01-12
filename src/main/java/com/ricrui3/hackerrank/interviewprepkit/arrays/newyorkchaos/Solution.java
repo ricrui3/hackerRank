f88643530cb682f568917fa759cb03e0f00b6d76
Ricardo Ruiz <ricrui3@gmail.com>
2019-01-12 16:51:28 -0600

    Final Solution of the 'New York Chaos' challenge

--- src/main/java/com/ricrui3/hackerrank/interviewprepkit/arrays/newyorkchaos/Solution.java
+++ src/main/java/com/ricrui3/hackerrank/interviewprepkit/arrays/newyorkchaos/Solution.java
@@ -10,28 +10,34 @@ public class Solution {
 
     // Complete the minimumBribes function below.
     static void minimumBribes(int[] q) {
-        int storage = 0;
         int bribingCount = 0;
+        boolean isPossible = true;
+        boolean isSwapped = false;
+
         for (int i = 0; i < q.length; i++) {
-            int currentVale = q[i];
-            int maximumBribing = i + 3;
-            if (currentVale > i + 1) {
-                if (currentVale == maximumBribing) {
-                    swap(q, i, maximumBribing - 1);
-                    bribingCount += 2;
-                } else {
-                    swap(q, i, i + 1);
-                    bribingCount++;
+            if (q[i] - i - 1 > 2) {
+                isPossible = false;
+                break;
+            }
+        }
+        if (isPossible) {
+            for (int j = 0; j < q.length - 1; j++) {
+                for (int i = 0; i < q.length - 1; i++) {
+                    if (q[i] > q[i + 1]) {
+                        swap(q, i, i + 1);
+                        isSwapped = true;
+                        bribingCount++;
+                    }
                 }
-            } else if (currentVale < i + 1) {
-                if (currentVale == i - 3) {
-                    swap(q, i, i - 3 - 1);
-                    bribingCount += 2;
+                if (isSwapped) {
+                    isSwapped = false;
                 } else {
-                    swap(q, i, i - 1);
-                    bribingCount++;
+                    break;
                 }
             }
+            System.out.println(bribingCount);
+        } else {
+            System.out.println("Too chaotic");
         }
     }
 
--- src/test/java/com/ricrui3/hackerrank/interviewprepkit/arrays/newyorkchaos/SolutionSpec.groovy
+++ src/test/java/com/ricrui3/hackerrank/interviewprepkit/arrays/newyorkchaos/SolutionSpec.groovy
@@ -1,23 +1,37 @@
 package com.ricrui3.hackerrank.interviewprepkit.arrays.newyorkchaos
 
+import org.junit.Rule
+import org.springframework.boot.test.rule.OutputCapture
 import spock.lang.Shared
 import spock.lang.Specification
 
 class SolutionSpec extends Specification {
+
+    @Rule
+    OutputCapture outputCapture = new OutputCapture()
+
     def "Unit test of the 'New York Chaos' challenge"() {
         when:
         Solution.minimumBribes(queue)
         then:
-        arr1.size() == 5
+        outputCapture.toString() == "${result}\n"
         where:
-        queue | result
-        arr1  | 3
-        arr2  | 'Too chaotic'
+        queue      | result
+        testCase0a | 3
+        testCase0b | 'Too chaotic'
+        testCase1a | 'Too chaotic'
+        testCase1b | 7
     }
 
     @Shared
-    int[] arr1 = [3, 1, 2, 5, 4]
+    int[] testCase0a = [2, 1, 5, 3, 4]
 
     @Shared
-    int[] arr2 = [2, 5, 1, 3, 4]
+    int[] testCase0b = [2, 5, 1, 3, 4]
+
+    @Shared
+    int[] testCase1a = [5, 1, 2, 3, 7, 8, 6, 4]
+
+    @Shared
+    int[] testCase1b = [1, 2, 5, 3, 7, 8, 6, 4]
 }
